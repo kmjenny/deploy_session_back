0e1627d95a9c03a8e29713352e9d14b0e5d59e15
ustar1210 <ays991210@naver.com>
2023-08-02 20:35:15 +0900

    corsheaders middleware 추가

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "project.urls"
