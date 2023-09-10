66eecc3d67e0b1f383e53f52b6afd05054bdd6bc
jenny <88226911+kmjenny@users.noreply.github.com>
2023-09-10 21:00:27 +0900

    yml 추가

--- project/settings.py
+++ project/settings.py
@@ -126,10 +126,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import os
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # CORS 설정
